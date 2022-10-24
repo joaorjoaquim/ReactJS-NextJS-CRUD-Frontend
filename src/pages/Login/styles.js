8586a089849204552bb1f7bde8b2c07662f32f3e
João Ricardo Joaquim <joaorjoaquim@gmail.com>
2022-10-24 18:02:07 -0300

    login

---
+++ src/pages/Login/styles.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    min-width: 100vw;
+`;
+
+export const Content = styled.div`
+    width: 100%;
+    max-width: 400px;
+    margin: 30px;
+    background: #fff;
+    border-radius: 4px;
+    padding: 20px;
+`;
+
+export const Form = styled.form`
+    display: flex;
+    padding: 3rem;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: #383290;
+    border-radius: 5px;
+    width: 100%;
+    max-width: 450px;
+    gap: 30px 0px;
+
+    h1 {
+        color: white;
+        font-size: 20px;
+        font-weight: light;
+    }
+
+    p {
+        color: white;
+        font-size: 16px;
+        font-weight: bold;
+    }
+
+    a {
+        color: white;
+        font-size: 14px;
+    }
+`;
