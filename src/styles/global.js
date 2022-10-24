aee739d12fb713539d47daf4c0a5f6c2c9a0b92a
João Ricardo Joaquim <joaorjoaquim@gmail.com>
2022-10-24 16:21:27 -0300

    global style and others

---
+++ src/styles/global.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        outline: 0;
+        box-sizing: border-box;
+    }
+    body {
+        font-family: 'Poppins', sans-serif;
+        font-size: 14px;
+        background: radial-gradient(circle, rgba(68,62,172,1) 0%, rgba(129,129,215,1) 0%, rgba(68,77,167,1) 100%);
+        text-rendering: optimizeLegibility;
+        -webkit-font-smoothing: antialiased;
+    }
+    html, body, #root {
+        height: 100%;
+    }
+`;
