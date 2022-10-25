7a778a5207d84bdd40c970acfdcbb1c02c30e448
João Ricardo Joaquim <joaorjoaquim@gmail.com>
2022-10-25 14:28:00 -0300

    css

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -3,11 +3,16 @@ import styled from 'styled-components';
 export const CustomButton = styled.button`
     color: #fff;
     font-size: 20px;
+    font-family: 'Poppins', sans-serif;
     border: 30px;
-    background-color: #6a6a6a;
+    background-color: #7274cb;
     border-radius: 8px;
     width: 100%;
     height: 50px;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
     cursor: pointer;
+
+    &:hover {
+        background-color: #5b61b9;
+    }
 `;
--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -2,15 +2,16 @@ import styled from 'styled-components';
 
 export const CustomInput = styled.input`
     color: #fff;
-    font-size: 20px;
+    font-size: 18px;
+    font-family: 'Poppins', sans-serif;
     background-color: transparent;
-    border: 2px solid #6a6a6a;
+    border: 2px solid #b7b3f5;
     border-radius: 8px;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
     padding: 16px 20px;
     width: 100%;
 
-    input::placeholder {
+    &::placeholder {
         color: #fff;
         font-size: 12px;
         opacity: 0.7;
