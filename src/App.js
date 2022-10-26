02e0364661296c6d38e0b76ccce2e7a0749015c4
João Ricardo Joaquim <joaorjoaquim@gmail.com>
2022-10-26 13:38:46 -0300

    fixing small things

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
     return (
         <div className="App">
             <GlobalStyle />
-            <AppRoutes />
+            <AppRoutes>
+                <Navbar />
+            </AppRoutes>
         </div>
     );
 }
