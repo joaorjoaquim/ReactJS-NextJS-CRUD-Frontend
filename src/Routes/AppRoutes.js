821c59881913c0789bf78eabb11efb02f4a9b785
João Ricardo Joaquim <joaorjoaquim@gmail.com>
2022-10-26 13:37:54 -0300

    updating routes

--- src/Routes/AppRoutes.js
+++ src/Routes/AppRoutes.js
@@ -3,6 +3,9 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Login from '../Pages/Login';
 import Home from '../Pages/Home';
 import ErrorPage from '../Pages/ErrorPage';
+import Users from '../Pages/Users';
+import Projects from '../Pages/Projects';
+import Tasks from '../Pages/Tasks';
 
 export default function AppRoutes() {
     const router = createBrowserRouter([
@@ -14,6 +17,21 @@ export default function AppRoutes() {
         {
             path: '/home',
             element: <Home />,
+            errorElement: <ErrorPage />,
+        },
+        {
+            path: '/users',
+            element: <Users />,
+            errorElement: <ErrorPage />,
+        },
+        {
+            path: '/projects',
+            element: <Projects />,
+            errorElement: <ErrorPage />,
+        },
+        {
+            path: '/tasks',
+            element: <Tasks />,
             errorElement: <ErrorPage />,
         },
     ]);
